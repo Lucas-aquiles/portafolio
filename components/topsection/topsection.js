14a68997f6ef2086f8e3c538614c515d25095bcf
Lucas-aquiles <lucasechegaray2011@gmail.com>
2022-10-17 02:44:06 -0300

    add section1

---
+++ components/topsection/topsection.js
@@ -0,0 +1,28 @@
+
+import Image from 'next/image';
+import styles from './topsection.module.css'
+import profilePic from '../../public/images/20.svg'
+
+
+export default function Topsection() {
+
+
+
+  return (
+    <div className={styles.container}>
+        <h1 className={styles.text}>Programming is the paradise of the curious</h1>
+        <Image
+        paddingLeft={100}
+    src={profilePic} // Route of the image file
+    // height={1000} // Desired size with correct aspect ratio
+    // width={1000} // Desired size with correct aspect ratio
+    alt="Your Name"
+  />
+
+    {/* < img src={imagenes}/>
+    < img  className='smoke1' src={imagenes2}/>
+    < img  className='smoke1 smoke2' src={imagenes2}/> */}
+
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Navbar from '../components/navbar/navbar'
+import Topsection from '../components/topsection/topsection'
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         <meta name="description" content="portafolio  web portfolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Topsection/>
 
       <Navbar/>
 
