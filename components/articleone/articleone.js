174aab98629af3e88fca6bc415d18905a9abc0eb
Lucas-aquiles <lucasechegaray2011@gmail.com>
2022-10-18 04:36:18 -0300

    change

--- components/articleone/articleone.js
+++ components/articleone/articleone.js
@@ -15,14 +15,14 @@ export default function ArticleOne() {
           />
       </div>
       <div className={styles.childText}>
-        {/* <p>
+        <p>
           Hey !! My name is Lucas. I&apos;m a developer from Argentina, Mendoza
           province. I have been working on both client end and server side. I am
           interested in creative development , accessible to the user . And also
           in a design , code structure that allows scalability , readability. I
           have the desire to do things that impact people&apos;s lives , improving it
           , helping , etc..
-        </p> */}
+        </p> 
         <div >
           {/* <img   className='icon'  src= { require('../imag/instagram.svg').default }  />
   <img src= { require('../imag/linkedin-original.svg').default }  />
