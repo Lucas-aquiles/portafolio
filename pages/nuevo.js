d1cd379d6720e74e47acc3a4d6924c75e363dd74
Lucas-aquiles <lucasechegaray2011@gmail.com>
2022-10-19 17:12:25 -0300

    add pdf

---
+++ pages/nuevo.js
@@ -0,0 +1,12 @@
+
+
+export default function Nuevo() {
+  return (
+
+    <a href='/assets/lucas.pdf' download="resume.pdf">
+    <button>
+      Download Resume
+    </button>
+  </a>    
+                      )
+}
