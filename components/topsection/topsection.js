2b8e56950107a9351cb7e9f12a7f913aae2dc80f
Lucas-aquiles <lucasechegaray2011@gmail.com>
2022-10-20 15:14:48 -0300

    changes

--- components/topsection/topsection.js
+++ components/topsection/topsection.js
@@ -1,49 +1,42 @@
-
-import Image from 'next/image';
-import styles from './topsection.module.css'
-import profilePic from '../../public/images/20.svg'
-import smoke from "../../public/images/smoke.svg"
+import Image from "next/image";
+import styles from "./topsection.module.css";
+import profilePic from "../../public/images/20.svg";
+import smoke from "../../public/images/smoke.svg";
 
 export default function Topsection() {
-
-
-
   return (
-    <div   id="home" className={styles.container}>
-        {/* <h1 className={styles.text}>Programming is the paradise of the curious</h1> */}
-      <div> 
+    <div id="home" className={styles.container}>
+      {/* <h1 className={styles.text}>Programming is the paradise of the curious</h1> */}
+      <div className={styles.imageWrapper}>
         <Image
-    src={profilePic} // Route of the image file
-    // height={1000} // Desired size with correct aspect ratio
-    // width={1000} // Desired size with correct aspect ratio
-    alt="Your Name"
-  />
-<div className={styles.smokeContainer}>  
-  <div className={styles.smoke1}>  
-     <Image
-    src={smoke} // Route of the image file
-    // height={1000} // Desired size with correct aspect ratio
-    // width={1000} // Desired size with correct aspect ratio
-    alt="Your Name"
-  />
-  </div>
-  <div className={styles.smoke2}>
- <Image
-    src={smoke} // Route of the image file
-    // height={1000} // Desired size with correct aspect ratio
-    // width={1000} // Desired size with correct aspect ratio
-    alt="Your Name"
-  />
-</div>
-</div>
-
-
-</div>
-
-
-  
-   
-
+          src={profilePic} // Route of the image file
+          // height={1000} // Desired size with correct aspect ratio
+          // width={1000} // Desired size with correct aspect ratio
+          layout="fill"
+          objectFit="contain"
+          alt="Your Name"
+        />
+      </div>
+      <div className={styles.divClon}>   
+      <div className={styles.smokeContainer}>
+        <div className={styles.smoke1}>
+          <Image
+            src={smoke} // Route of the image file
+            // height={1000} // Desired size with correct aspect ratio
+            // width={1000} // Desired size with correct aspect ratio
+            alt="Your Name"
+          />
+        </div>
+        <div className={styles.smoke2}>
+          <Image
+            src={smoke} // Route of the image file
+            // height={1000} // Desired size with correct aspect ratio
+            // width={1000} // Desired size with correct aspect ratio
+            alt="Your Name"
+          />
+        </div>
+      </div>
+      </div>
     </div>
-  )
+  );
 }
